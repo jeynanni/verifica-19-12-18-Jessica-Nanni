9378f7f4767996e14e3c8db886b0f74835fda902
unknown <FrontEnd@PCAU0936.formart.it>
2018-12-19 13:11:24 +0100

    five commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 import Body from './components/body/Body';
-
+import { Alert } from 'reactstrap';
+import Loader from 'react-loader-spinner';
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class App extends Component {
     this.state = {
       albums : [],
       photo : [],
-      
+      loader : true
     };
   }
 
@@ -24,30 +24,38 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/albums' )// fetch for albums
       .then(response => response.json())
       .then(json => this.setState({
-        albums:json
+        albums:json,
+        
   }))
 }
 photo() {
     fetch('https://jsonplaceholder.typicode.com/photos')// fetch for photos
       .then(response => response.json())
       .then(json => this.setState({
-        photo:json
+        photo:json,
+        loader : false
   }))
 }
 
   render() {
     return (
       <div className="App">
+      <Alert color="info">
+       <h1><i> Single Page Application</i></h1>
+      </Alert>
         <div>
           {this.state.photo.map((item,index)=> {
             if(index<10){
               return <Body key={'photo-list'+ index}  title={item.title} url={item.url} thumbnail={item.thumbnailUrl}user={item.albumId}id= {item.id}/>
             }
           })}
-       
+       {this.state.loader && <Loader type="TailSpin" color="#ffff" height={80} width={80} />}
         {/*<Photos thumbnail={this.state.photo.thumbnailUrl} title={this.state.photo.title}/><Photos url ={this.state.photo.url}/> ======>single image*/}
        {/*} {this.state.albums.map((item,index) => (<Body key={'photo-list' + index} user={item.albumId} id= {item.id} title={item.title}/>))}*/}
       </div>
+      <Alert color="dark">
+        <h4><b>made by Jessica Nanni </b></h4>
+      </Alert>
       </div>
     );
   }
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Body.css';
+import { Media } from 'reactstrap';
 
 class Body extends Component {
     
@@ -9,13 +10,21 @@ class Body extends Component {
   render() {
     return (
       <div className="Body">
+      <div className ="container">
+      <div className ="firstImg">
+      <img alt={this.props.title} src={this.props.thumbnail}/>
+      </div>
+      <div className="second">
       <div>{this.props.albumId}<div/>
       <div>{this.props.id}</div>
       <div>{this.props.title}</div>
+      </div>
       <img alt={this.props.title} src={this.props.url}/>  
-      <img alt={this.props.title} src={this.props.thumbnail}/>
       </div>
       </div>
+      
+      </div>
+      
     );
   }
 }
